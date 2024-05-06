ca3b8638e6eff34af9c844e0743c62bad75626e5
Majesticaxt <158748256+Majesticaxt@users.noreply.github.com>
2024-05-06 18:21:41 +0100

    Update client.js

--- src/client.js
+++ src/client.js
@@ -3,3 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ioynkpwscfbxocvvarzc.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlveW5rcHdzY2ZieG9jdnZhcnpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTI2Njk1NTcsImV4cCI6MjAyODI0NTU1N30.TXEFSm1UXVK51iNUL5JHPlfFPWvCoVlU0gj2G7NOaD8'
 export const supabase = createClient(supabaseUrl, supabaseKey)
+// changes from github
